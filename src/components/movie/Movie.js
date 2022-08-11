5adf95ecf06643de7b820b57c37931718d77d414
JenThomasJames <jenthomasjames123@gmail.com>
2022-08-11 14:54:40 +0530

    Added watch now button to movie card

--- src/components/movie/Movie.js
+++ src/components/movie/Movie.js
@@ -1,6 +1,12 @@
 import styles from "./Movie.module.css";
+import Button from "../../shared/Button";
 const Movie = (props) => {
-  const rating = (Math.random() * 10 + 1).toFixed(2);
+  const rating = (Math.random() * 5 + 5).toFixed(1);
+
+  const watchNowHandler = () => {
+    console.log("Watching Now: " + props.movie.title);
+  };
+
   return (
     <div className={styles["frosty-card"]}>
       <div className={styles["card-data"]}>
@@ -9,6 +15,9 @@ const Movie = (props) => {
           <span className={styles["rating-badge"]}>
             <h4>{rating}</h4>
           </span>
+        </div>
+        <div className={styles["card-footer"]}>
+          <Button btnText="Watch Now" clickHandler={watchNowHandler} />
         </div>
         <p>{props.movie.opening_crawl}</p>
       </div>
---
+++ src/shared/Modal.js
@@ -0,0 +1,37 @@
+import styles from "./Modal.module.css";
+import { Fragment } from "react";
+import ReactDOM from "react-dom";
+
+const Backdrop = () => {
+  return <div className={styles.backdrop}></div>;
+};
+
+const ModalDialog = (props) => {
+  return (
+    <div className={styles.modal}>
+      <div>{props.children}</div>
+    </div>
+  );
+};
+
+const Modal = (props) => {
+  const overlayPortal = document.getElementById("overlays");
+
+  return (
+    <Fragment>
+      {ReactDOM.createPortal(
+        <ModalDialog>
+          <div className={styles["modal-body"]}>
+            <button className={styles.close} onClick={props.onHideModal}>
+              &times;
+            </button>
+            {props.children}
+          </div>
+        </ModalDialog>,
+        overlayPortal
+      )}
+      {ReactDOM.createPortal(<Backdrop />, overlayPortal)}
+    </Fragment>
+  );
+};
+export default Modal;

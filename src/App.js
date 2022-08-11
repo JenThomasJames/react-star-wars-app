4c69fc5792d34ec95573b665ee98f1ecd7c2ca8c
JenThomasJames <jenthomasjames123@gmail.com>
2022-08-11 16:37:01 +0530

    Added loading spinner

--- src/App.js
+++ src/App.js
@@ -3,19 +3,24 @@ import "./App.css";
 import Movies from "./components/movies/Movies";
 import Button from "./shared/Button";
 import Card from "./shared/Card";
+import Spinner from "./shared/Spinner";
 
 function App() {
   const [data, setData] = useState({});
+  const [showSpinner, setShowSpinner] = useState(false);
   const fetchMoviesHandler = async () => {
+    setShowSpinner(true);
     try {
       const response = await fetch("https://swapi.dev/api/films/");
       if (!response.ok) {
+        setShowSpinner(false);
         throw new Error("Something went wrong :(");
       }
       setData(await response.json());
     } catch (error) {
       console.log(error.message);
     }
+    setShowSpinner(false);
   };
 
   return (
@@ -31,6 +36,11 @@ function App() {
         </Fragment>
       )}
       {Object.keys(data).length !== 0 && <Movies movieData={data.results} />}
+      {showSpinner && (
+        <div className="center-all">
+          <Spinner />
+        </div>
+      )}
     </div>
   );
 }

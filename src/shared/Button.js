ec7dfc7c34fa3a5e6a9214bb6e62302631768dbc
JenThomasJames <jenthomasjames123@gmail.com>
2022-08-11 10:06:08 +0530

    Added Button Template

---
+++ src/shared/Button.js
@@ -0,0 +1,11 @@
+import { Fragment } from 'react';
+import styles from './Button.module.css';
+
+const Button = props => {
+    return (
+        <Fragment>
+
+        </Fragment>
+    );
+}
+export default Button;
